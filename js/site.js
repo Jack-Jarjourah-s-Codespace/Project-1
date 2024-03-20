3711f5519f96b64a39b44afd16e9c9c5a119d73a
PolarBear78 <56362723+PolarBear78@users.noreply.github.com>
2024-03-20 15:03:03 -0500

    Finalization and Formatting
    -Added proper dedicated mobile sizing.
    -Formatted and indented
    -Finalized Javascript.

--- js/site.js
+++ js/site.js
@@ -1,19 +1,12 @@
-
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
+jQuery(function ($) {
+  $("html").removeClass("nojs");
+  $("html").addClass("hasjs");
 });
 
-function validateEmail(email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function ValidateEmail(email) {
+  var re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
-var emailInput = document.getElementById('email');
-emailInput.addEventListener('input', function (event) {
-  if (!validateEmail(this.value)) {
-    event.target.setCustomValidity('Invalid email format');
-  } else {
-    event.target.setCustomValidity('');
-  }
-});
+
